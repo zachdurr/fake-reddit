172e725fa2e788e82cfc07a68e922bd2ebb1444b
zdurr22 <zdurr22@gmail.com>
2022-01-21 13:15:49 -0500

    set up graphql server

--- dist/index.js
+++ dist/index.js
@@ -4,14 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
-    const post = orm.em.create(Post_1.Post, { title: 'my first post' });
-    await orm.em.persistAndFlush(post);
-    console.log('--------------sql2-----------------');
-    await orm.em.nativeInsert(Post_1.Post, { title: 'my first post 2' });
+    await orm.getMigrator().up();
+    const app = (0, express_1.default)();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [hello_1.HelloResolver],
+            validate: false
+        })
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000');
+    });
 };
 main();
 //# sourceMappingURL=index.js.map
